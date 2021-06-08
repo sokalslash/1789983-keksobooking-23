5b9b3d9a5f326d7f577b64ea7a02c095f21f30ce
Mary Lisitsa <sokalslash@mail.ru>
2021-06-08 14:59:47 +0300

    Исправляет ошибки

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
-
-function getRandomIntInclusive (min, max) {
-  if (min < 0 || max < 0) {
-    return 'введите положительное число';
-  }
-  if (max <= min) {
-    return 'введите число больше начального';
-  }
-  // источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-getRandomIntInclusive(0.5, 1.6);
-
-function getRandomFloat (min, max, numberDecimalPlaces) {
-  if (min < 0 || max < 0) {
-    return 'введите положительное число';
-  }
-  if (max <= min) {
-    return 'введите число больше начального';
-  }
-  const randomNumber = Math.random() * (max - min + 0.1) + min;
-  return  (+randomNumber.toFixed(numberDecimalPlaces));
-}
-getRandomFloat(0.5, 3.3, 2);
-
 const TITLES = [
   'Прекрасный вид из окна',
   'Есть электрокамин',
@@ -68,37 +41,62 @@ const PHOTOS = [
 
 const SIMILAR_ADVERTISEMENT_COUNT = 10;
 
-function createAdvertisement () {
-  const getRandomArrayElement = (elements) =>  elements[getRandomIntInclusive(0, elements.length - 1)];
+const getRandomIntInclusive = (min, max) => {
+  if (min < 0 || max < 0) {
+    return 'введите положительное число';
+  }
+  if (max <= min) {
+    return 'введите число больше начального';
+  }
+  // источник https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-  const getArrayOfStrings = (strings) => new Array(getRandomIntInclusive(1, strings.length)).fill(null).map(() =>
-    strings[getRandomIntInclusive(0, strings.length-1)],
-  );
+const getRandomFloat = (min, max, numberDecimalPlaces) => {
+  if (min < 0 || max < 0) {
+    return 'введите положительное число';
+  }
+  if (max <= min) {
+    return 'введите число больше начального';
+  }
+  const randomNumber = Math.random() * (max - min + 0.1) + min;
+  return  (+randomNumber.toFixed(numberDecimalPlaces));
+};
 
-  return {
-    author: {
-      avatar: `img/avatars/user0${  getRandomIntInclusive(1, 8)  }.png`,
-    },
-    offer: {
-      title: TITLES[getRandomIntInclusive(0, TITLES.length-1)],
-      address: `location.${  getRandomFloat(35.65000, 35.70000, 5)  }, location.${  getRandomFloat(139.70000, 139.80000, 5)}`,
-      price: getRandomIntInclusive(100, 1000),
-      type: getRandomArrayElement(TYPES),
-      rooms: getRandomIntInclusive(1, 10),
-      guests: getRandomIntInclusive(1, 20),
-      checkin: getRandomArrayElement(WATCHES),
-      checkout: getRandomArrayElement(WATCHES),
-      features: getArrayOfStrings(FEATURES),
-      description: getRandomArrayElement(DESCRIPTIONS),
-      photos: getArrayOfStrings(PHOTOS),
-    },
-    location: {
-      lat: getRandomFloat(35.65000, 35.70000, 5),
-      lng: getRandomFloat(139.70000, 139.80000, 5),
-    },
+const getRandomArrayElement = (elements) =>  elements[getRandomIntInclusive(0, elements.length - 1)];
 
-  };
-}
+const getArrayOfElement = (strings) => new Array(getRandomIntInclusive(1, strings.length)).fill(null).map(() =>
+  strings[getRandomIntInclusive(0, strings.length-1)],
+);
+
+const locationAddressLat = getRandomFloat(35.65000, 35.70000, 5);
+const locationAddressLng = getRandomFloat(139.70000, 139.80000, 5);
+
+const createAdvertisement = () => ({
+  author: {
+    avatar: `img/avatars/user0${  getRandomIntInclusive(1, 8)  }.png`,
+  },
+  offer: {
+    title: TITLES[getRandomIntInclusive(0, TITLES.length-1)],
+    address: `location.${  locationAddressLat  }, location.${  locationAddressLng}`,
+    price: getRandomIntInclusive(100, 1000),
+    type: getRandomArrayElement(TYPES),
+    rooms: getRandomIntInclusive(1, 10),
+    guests: getRandomIntInclusive(1, 20),
+    checkin: getRandomArrayElement(WATCHES),
+    checkout: getRandomArrayElement(WATCHES),
+    features: getArrayOfElement(FEATURES),
+    description: getRandomArrayElement(DESCRIPTIONS),
+    photos: getArrayOfElement(PHOTOS),
+  },
+  location: {
+    lat: locationAddressLat,
+    lng: locationAddressLng,
+  },
+
+});
 
 const similarAdvertisement = new Array(SIMILAR_ADVERTISEMENT_COUNT).fill(null).map(() => createAdvertisement());
 similarAdvertisement;
