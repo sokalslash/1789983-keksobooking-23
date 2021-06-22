c8f24b5b446c585039786df778639cac7131fb6f
Mary Lisitsa <sokalslash@mail.ru>
2021-06-22 21:15:09 +0300

    Валидация формы подачи объявления

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import {creatDataForAds} from './data.js';
 import {createSimilarAd} from './popup.js';
+import './validation.js';
 const mapBlock = document.querySelector('#map-canvas');
 const dataForAds = creatDataForAds();
 const similarAd = createSimilarAd(dataForAds[0]);
