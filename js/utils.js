d2733dc68c2173ac238a3857d03f8a6f134f552e
Mary Lisitsa <sokalslash@mail.ru>
2021-07-15 19:47:14 +0300

    Правка по дополнительному критерию

--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,11 @@
 const isEscEvent = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
 
-function debounce (callback, timeoutDelay = 500) {
+const debounce = (callback, timeoutDelay = 500) => {
   let timeoutId;
   return (...rest) => {
     clearTimeout(timeoutId);
     timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
   };
-}
+};
 
 export {isEscEvent, debounce};
